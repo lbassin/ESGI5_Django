d8118119e7db33fa0cb3758ca58a65691a8d1d09
Laurent Bassin <laurent@bassin.info>
2018-11-11 16:20:22 +0100

    Edit deck

--- cards/views.py
+++ cards/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 
 from cards.forms import CreateDeckForm
+from cards.models import Deck
 from users.models import Profile
 
 
@@ -14,18 +15,18 @@ def cards(request):
     page = request.GET.get('page')
     displayed_card = paginator.get_page(page)
 
-    return render(request, 'users/cards.html', {'cards': displayed_card})
+    return render(request, 'cards/cards.html', {'cards': displayed_card})
 
 
 @login_required
 def decks(request):
     deck_list = Profile.object.get_decks(request.user)
 
-    return render(request, 'users/decks.html', {'decks': deck_list})
+    return render(request, 'cards/decks.html', {'decks': deck_list})
 
 
 @login_required
-def decks_new(request):
+def deck_new(request):
     form = CreateDeckForm(request.POST if request.method == 'POST' else None)
     for key in form.fields.keys():
         form.fields[key].widget.attrs.update({'class': 'form-control'})
@@ -37,4 +38,22 @@ def decks_new(request):
 
             return redirect('decks')
 
-    return render(request, 'users/decks_new.html', {'form': form})
+    return render(request, 'cards/decks_new.html', {'form': form})
+
+
+@login_required
+def deck_edit(request, id):
+    deck = Deck.objects.filter(id=id).get()
+    form = CreateDeckForm(request.POST if request.method == 'POST' else None, instance=deck)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.user = request.user
+            form.save()
+
+            return redirect('decks')
+
+    for key in form.fields.keys():
+        form.fields[key].widget.attrs.update({'class': 'form-control'})
+
+    return render(request, 'cards/decks_edit.html', {'form': form})
