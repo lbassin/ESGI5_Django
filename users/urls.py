f786eb0dab008d68457c59bc183c2435743f1d8f
Laurent Bassin <laurent@bassin.info>
2018-11-11 12:20:23 +0100

    Move deck functions to cards bundle

--- users/urls.py
+++ users/urls.py
@@ -6,8 +6,5 @@ from . import views
 urlpatterns = [
     url('', include('django.contrib.auth.urls')),
     url('dashboard', views.dashboard, name='dashboard'),
-    url('register', views.register, name='register'),
-    url('cards', views.cards, name='users_cards'),
-    url('decks/new', views.decks_new, name='users_decks_new'),
-    url('decks', views.decks, name='users_decks')
+    url('register', views.register, name='register')
 ]
