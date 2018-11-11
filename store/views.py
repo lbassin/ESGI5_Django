d684eae301109e837b1a1dd249cba70988330373
Laurent Bassin <laurent@bassin.info>
2018-11-11 17:52:01 +0100

    Buy cards

--- store/views.py
+++ store/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
-
 # Create your views here.
 from django.shortcuts import render
 
+from cards.models import Card
 from store.models import Package
 
 
@@ -11,3 +10,18 @@ def store(request):
     user_credits = request.user.profile.credits
 
     return render(request, 'store/store.html', {'packages': packages, 'user_credits': user_credits})
+
+
+def buy(request):
+    profile = request.user.profile
+    package = Package.objects.get(id=request.POST.get('id'))
+    if package.price > profile.credits:
+        return render(request, 'store/buy.html', {'success': False})
+
+    cards = Card.objects.get_random(package.cards_count)
+
+    profile.cards.add(*cards)
+    profile.credits -= package.price
+    profile.save()
+
+    return render(request, 'store/buy.html', {'success': True, 'package': package, 'cards': cards})
